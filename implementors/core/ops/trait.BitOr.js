03676f56d4ecc6ff9a1126550251836a9ef1f5af
Johan Sköld <johan@skold.cc>
2016-01-07 09:39:45 -0800

    Documentation pages

---
+++ implementors/core/ops/trait.BitOr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['bgfx'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a> for <a class='struct' href='bgfx/struct.StateFlags.html' title='bgfx::StateFlags'>StateFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a> for <a class='struct' href='bgfx/struct.BufferFlags.html' title='bgfx::BufferFlags'>BufferFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a> for <a class='struct' href='bgfx/struct.ClearFlags.html' title='bgfx::ClearFlags'>ClearFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a> for <a class='struct' href='bgfx/struct.DebugFlags.html' title='bgfx::DebugFlags'>DebugFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a> for <a class='struct' href='bgfx/struct.ResetFlags.html' title='bgfx::ResetFlags'>ResetFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['bgfx'] = ["impl&lt;'s&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='bgfx/struct.Program.html' title='bgfx::Program'>Program</a>&lt;'s&gt;","impl&lt;'m&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='bgfx/struct.Shader.html' title='bgfx::Shader'>Shader</a>&lt;'m&gt;","impl&lt;'m&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='bgfx/struct.IndexBuffer.html' title='bgfx::IndexBuffer'>IndexBuffer</a>&lt;'m&gt;","impl&lt;'m&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='bgfx/struct.VertexBuffer.html' title='bgfx::VertexBuffer'>VertexBuffer</a>&lt;'m&gt;","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='bgfx/struct.Bgfx.html' title='bgfx::Bgfx'>Bgfx</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
